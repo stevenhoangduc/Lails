60ab5585735c5566b9005ff504d4f71eb0a4c489
stevenhoangduc <hoangducsteven@gmail.com>
2025-01-28 19:41:28 -0800

    delete comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -20,7 +20,21 @@ router.post ('/users/:userId/lails/:lailId/comments', async function (req, res){
     res.send("Error check the terminal to debug");
   }
 }) 
+router.delete ('/users/:userId/lails/:lailId/comments/:commentId', async function (req, res){
+    try {
+        const owner = await UserModel.findById(req.params.userId)
+        const lail = owner.lails.id(req.params.lailId)
+        const deleteComment= await Comment.findByIdAndDelete(req.params.commentId)
+        const index = lail.comments.indexOf(deleteComment._id)
+        lail.comments.splice(index, 1)
+        await owner.save()
+        res.redirect(`/users/${owner._id}/lails/${lail._id}`)
 
+    }catch (err) {
+    console.log(err);
+    res.send("Error check the terminal to debug");
+    }
+})
 
 
 
